14d0e4fdf9f4eb9680bf5ed75193e6975eeadf0b
Ziad <ziadzayed7@gmail.com>
2022-01-26 19:24:12 +0300

    create models and DB.tables

--- makeup/models.py
+++ makeup/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.class Question(models.Model):
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    orgin = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=50)
+    descreption = models.CharField(max_length=250)
+    expirDate= models.DateField()
+    price=models.IntegerField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
